7fb93a0a66e686e6037350dad721304266576fba
DiegoTeranRosales <75071676@ucsm.edu.pe>
2021-07-16 20:33:42 -0500

    Se implemento grafos para poder añadir una funcionalidad al sistema para poder adquirir la direccion mas cercana de un hospital mediante los distritos(Todo el grupo a conttribuido con la implementacion de esta parte del codigo)

--- Verificador-Hash/src/ListLinked.java
+++ Verificador-Hash/src/ListLinked.java
@@ -1,37 +1,37 @@
-public class ListLinked<T>implements TDAList<T>  {
+public class ListLinked<T>  {
 
-	public Node<T> first;
-	private Node<T> sorted;
+    public Node<T> first;
+    private Node<T> sorted;
 
-	int count;
+    int count;
 
-	public ListLinked() {
-		this.first =null;//head
-		this.count = 0;//contador
+    public ListLinked() {
+        this.first =null;//head
+        this.count = 0;//contador
 
-	}
+    }
 
-	public Node<T> getFirst() {
-		return first;
-	}
+    public Node<T> getFirst() {
+        return first;
+    }
 
-	public void setFirst(Node<T> first) {
-		this.first = first;
-	}
+    public void setFirst(Node<T> first) {
+        this.first = first;
+    }
 
-	public boolean isEmptyList() {
-		return this.first == null;
-	}
+    public boolean isEmptyList() {
+        return this.first == null;
+    }
 
-	public int lenght() {
-		return this.count;
-	}
+    public int lenght() {
+        return this.count;
+    }
 
-	public void destroyList() {
-		while(this.first != null)
-			this.first = this.first.getNext();
-		this.count = 0;
-	}
+    public void destroyList() {
+        while(this.first != null)
+            this.first = this.first.getNext();
+        this.count = 0;
+    }
 
     void sortedInsert(Node<T> newnode) 
     {
@@ -54,21 +54,23 @@ public class ListLinked<T>implements TDAList<T>  {
         }
     }
 
-	//prerequisito
-	private Node<T> getLastNode(){
-		Node<T> aux = this.first;
-		while(aux.getNext() != null)
-			aux = aux.getNext();
-		return aux;
-	}
+    //prerequisito
+    private Node<T> getLastNode(){
+        Node<T> aux = this.first;
+        while(aux.getNext() != null)
+            aux = aux.getNext();
+        return aux;
+    }
 
-	public String toString() {
-		String str="";
-		Node<T> aux = this.first;
-		for (int i=0; aux != null; aux = aux.getNext(), i++)
-			str += "["+i+"]"+"\t"+ aux.getData()+"\n";
-		return str;
-	}
+    public String toString() {
+        String r="";
+        Node<T> aux = this.first;
+        while(aux != null) {
+            r=r+aux.getInfo();
+            aux=aux.getNext();
+        }
+        return r;
+    }
 
 
 
@@ -80,73 +82,78 @@ public class ListLinked<T>implements TDAList<T>  {
         this.count++;
     }
 
+    public void remove(T key)
+    {
+        Node<T> temp = this.first, prev = null;
+ 
+        if (temp != null && temp.getData().equals(key)) {
+            this.first = temp.getNext(); // Changed head
+            return;
+        }
+ 
+        while (temp != null && temp.getData().equals(key)) {
+            prev = temp;
+            temp = temp.getNext();
+        }
+ 
+        if (temp == null)
+            return;
+        prev.setNext(temp.getNext());
+    }
 
 
-	public void insertLast(T x) {
-	if(this.isEmptyList())
-		this.insertFirst(x);
-	else {
-		Node<T> lastNode = getLastNode();
-		lastNode.setNext(new Node<T>(x));
-		this.count++;
-		}
-	}
 
-	public boolean search(T x) {
+    public void insertLast(T x) {
+    if(this.isEmptyList())
+        this.insertFirst(x);
+    else {
+        Node<T> lastNode = getLastNode();
+        lastNode.setNext(new Node<T>(x));
+        this.count++;
+        }
+    }
+
+    public boolean search(T x) {
 
         Node<T> current = first;    //Iniciar actual
         while (current != null)
         {
             if (current.getData() == x)
-                return true;    
+                return true;
             current = current.getNext();
         }
-        return false;    
-    }	
+        return false;
+    }
+
+    public T searchData(T data) {
+        Node<T> nodo =this.first;
+        while (nodo!= null && !nodo.getData().equals(data)) {
+            nodo=nodo.getNext();
+        }
+        if (nodo!=null)
+            return nodo.getData();
+        return null;
+
+    }
 
 
 
-
-		public Integer ocurrencias(T x)
-	    {
-	        Node<T> current = this.first;
-	        Integer count = 0; /* index of Node we are
-	                          currently looking at */
-	        while (current != null)
-	        {
-	            if (count.equals(x ))
-	                return count;
-	            count++;
-	            current = current.getNext();
-	        }
-			return count;
-
+        public Integer ocurrencias(T x)
+        {
+            Node<T> current = this.first;
+            Integer count = 0; /* index of Node we are
+                              currently looking at */
+            while (current != null)
+            {
+                if (count.equals(x ))
+                    return count;
+                count++;
+                current = current.getNext();
+            }
+            return count;
 
 
-	}
 
-		@Override
-		public int search2(T x) {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		@Override
-		public void remove(T x) {
-	        Node<T> aux=this.first;
-	        int pos;
-	        int posi;
-	        int contador=0;
-	        pos=search2(x);
-	        posi=pos-1;
-	        while(contador<posi) {
-	            aux=aux.getNext();
-	            contador++;
-	        }
-	        aux.setNext(aux.getNext().getNext());
-	        this.count--;
-
-	    }
-
+    }
 
 }
