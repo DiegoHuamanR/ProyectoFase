22526208b20860c6fbfa525decc7b9fe0e1f1110
LibniHuaranga <73641532@ucsm.edu.pe>
2021-07-16 20:04:23 -0500

    Se implemento grafos para poder añadir una funcionalidad al sistema para poder adquirir la direccion mas cercana de un hospital mediante los distritos(Todo el grupo a conttribuido con la implementacion de esta parte del codigo)

--- PillBank3raFase/src/Node.java
+++ PillBank3raFase/src/Node.java
@@ -1,23 +1,48 @@
+import java.util.List;
 
-public class Node<T> {
-	public T data;
-	private Node<T> next;
+public class Node<Type> {
+	private Type data;//info
+	private Node<Type> next;//puntero
 	
-	public Node(T data) {
-		this(data,null);
-	}
-	public Node(T data,Node<T>next) {
+	boolean visited;
+
+   void visit() {
+        visited = true;
+    }
+
+    void unvisit() {
+        visited = false;
+    }
+	
+	
+	public Node(Type data ) {
 		this.data=data;
-		this.next=next;
+		this.next=null;
 	}
-	public T getData() {return this.data;}
-	public Node<T>getNext(){return this.next;}
-	public void setData(T data) {this.data = data;}
-	public void setNext(Node<T> next) {this.next=next;}
+	public Node(Type data, Node<Type> next) {
+		this.data=data;
+		this.next= next;
+	}
 	
+    public Node<Type> head = null;
+    public Node<Type> tail = null;
+    
+    public Type getInfo() {return data;}
+	public Type getData() { return this.data;}
+	public Node<Type> getNext(){return this.next;}
+	public void setData(Type data) {this.data=data;}
+	public void setNext(Node<Type> next) {this.next=next;}
+	
+	 public void displayData(){
+	      System.out.print(this.data + " ");
+	    }
 	
 	public String toString() {
 		return this.data.toString();
 	}
 
+	public boolean isVisited() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
--- PillBank3raFase/src/Test.java
+++ PillBank3raFase/src/Test.java
@@ -8,29 +8,83 @@ public class Test {
 	public static void main(String[] args) throws IOException {
 		HashUsuario<String> h= new HashUsuario<String>(10);
 		System.out.println("----------------------------------");
-		System.out.println("---------Prueba cuadratica----------");
+		System.out.println("---------Plegable----------");
 		System.out.println("---------Empleados----------");
 		h.insertplegable(73641532,"Libni","Bored","Andres Avelino Caceres","nini4578@hotmail.com",973625325,19,"nini");
 		h.insertplegable(75122532,"Diego","Arboleda","Jose Luis Bustamante y Rivero","diego8@hotmail.com",97364325,25,"nini");
 		h.insertplegable(75124212,"Rosa","Cespedes","Arayanes Umcacoyo","rosa78@hotmail.com",975325325,23,"nini");
 		System.out.println(h);
-		Verificador<String> pasti = new Verificador<String>(50);
+		Scanner entrada= new Scanner(System.in);
+        int opcionEligeUsuario=1;
+        while (opcionEligeUsuario != 3 ) {
+    		System.out.println("MENU DE PROGRAMA PILLBANK");
+    		System.out.println("1.  Buscar receta");
+    		System.out.println("2.  Hospital mas cercano");
+    		System.out.println("3.  Salir");
+    		System.out.println("Escoja Opción:");
+        	opcionEligeUsuario=entrada.nextInt();
 
-		pasti.insertR(pasti);
-        int resp = 1;
-        System.out.println("////////SEPA SU RECETA CON EL CÓDIGO DE RECETA////////\n");
-        while(resp == 1) {
-            System.out.print("Introduzca el código de la receta: ");
-            Scanner sc = new Scanner (System.in);
-            int code = sc.nextInt(); 
-            if(pasti.search(code) == 0) {
-                System.out.println("Código no existente.");
-                System.out.print("[1]->SI   [2]->NO  ");
-                System.out.print("¿Volver a ingresar código?: ");
-                Scanner x = new Scanner (System.in);
-                resp = x.nextInt();
-            }else {break;} 
+            if (opcionEligeUsuario == 1) {
+        		Verificador<String> pasti = new Verificador<String>(50);
+
+        		pasti.insertR(pasti);
+                int resp = 1;
+                System.out.println("////////SEPA SU RECETA CON EL CÓDIGO DE RECETA////////\n");
+                while(resp == 1) {
+                    System.out.print("Introduzca el código de la receta: ");
+                    Scanner sc = new Scanner (System.in);
+                    int code = sc.nextInt(); 
+                    if(pasti.search(code) == 0) {
+                        System.out.println("Código no existente.");
+                        System.out.print("[1]->SI   [2]->NO  ");
+                        System.out.print("¿Volver a ingresar código?: ");
+                        Scanner x = new Scanner (System.in);
+                        resp = x.nextInt();
+                    }else {break;} 
+                }
+            }
+
+
+            if (opcionEligeUsuario == 2) {
+            	  GraphLink<String>g=new GraphLink<String>();
+                  g.insertVertex("Hunter");
+                  g.insertVertex("Bustamante");
+                  g.insertVertex("Paucarpata");
+                  g.insertVertex("Yanahuara");
+                  g.insertVertex("CColorado");
+                  g.insertEdge("Hunter","Bustamante",16);
+                  g.insertEdge("Hunter","Paucarpata",25);
+                  g.insertEdge("Hunter","Yanahuara",20);
+                  g.insertEdge("Hunter","CColorado",22);
+                  g.insertEdge("Bustamante","Paucarpata",15);
+                  g.insertEdge("Bustamante","CColorado",23);
+                  g.insertEdge("Bustamante","Yanahuara",18);
+                  g.insertEdge("Paucarpata","CColorado",24);
+                  g.insertEdge("Paucarpata","Yanahuara",29);
+                  g.insertEdge("Yanahuara","CColorado",34);
+
+                  System.out.println("Grafo ciudades:\n"+g);
+                  int[ ] lista;
+                  lista = new int[20];
+                  lista=g.shortpath("CColorado","Yanahuara");
+                  for(int i=0;i<lista.length;i++) {
+                      if(lista[i]!=0) {
+                          System.out.println("Shortpath: "+lista[i]);
+                      }
+              } 
+            }
+            	
+
+           
+            if (opcionEligeUsuario == 3) {
+
+
+            } 
+            if (opcionEligeUsuario == 4) {      
+            	
+            }
+          }
         }
-	}
+	
 	
 }
